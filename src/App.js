6ed35c979d69458573c4a0c09bec2e0579657f8a
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-20 23:01:53 -0500

    submitted solution

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [toDos, setToDos] = useState([]);
 
+  // Update this app to retrieve a list of ToDo's from the jsonplaceholder API.
   // Load data from https://jsonplaceholder.typicode.com/todos?userId=3
+  // ToDo's for userID 3 are loaded using fetch() and useEffect()
+  // useEffect() returns a cleanup function that will cancel the fetch()
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function loadToDos() {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/todos?userId=3`,
+          { signal: abortController.signal }
+        );
+        const toDosFromAPI = await response.json();
+        setToDos(toDosFromAPI);
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Aborted", toDos);
+        } else {
+          throw error;
+        }
+      }
+    }
+
+    loadToDos();
+
+    return () => abortController.abort();
+  }, []);
 
   return (
     <div className="App">
